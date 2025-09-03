bd4279f1d71fee7563abd1b599956dbbfdb7a14e
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-09-03 08:48:58 -0300

    :sparkles: feat: Criando rota Get para a ação criada no repository de buscar todos e buscar por item

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,30 @@
 import { Router } from "express";
 
-import { Category } from "../model/category.js";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository()
 
-const categories: Category[] = [] 
 
 categoriesRoutes.post("/", (request, response)=>{
     const {name, description} = request.body;
 
-    
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-    return response.status(201).json({category});
+    if(categoryAlreadyExists){
+        return response.status(400).json({error: "Category already Exists"})
+    }
+
+    categoriesRepository.create({name, description});
+
+    return response.status(201).send();
+})
+
+categoriesRoutes.get("/", (request,response)=>{
+    const all = categoriesRepository.list()
+
+    return response.json(all)
 })
 
 export {categoriesRoutes}
