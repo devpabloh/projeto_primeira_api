dfb9e06981554019b47a79b01597264459f958c3
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-09-11 11:16:09 -0300

    ":sparkles: feat: Criando rota para categories"

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {CategoriesRepository} from "../modules/cars/repositories/CategoriesRepository.js"
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService.js";
+import { CreateCategoryUseCase } from "../modules/cars/useCases/createCategory/CreateCategoryUseCase.js";
 
 
 const categoriesRoutes = Router();
@@ -8,13 +8,7 @@ const categoriesRepository = new CategoriesRepository()
 
 
 categoriesRoutes.post("/", (request, response)=>{
-    const {name, description} = request.body;
-
-    const createCategoryService = new CreateCategoryService(categoriesRepository)
-
-    createCategoryService.execute({name, description})
-
-    return response.status(201).send();
+    
 })
 
 categoriesRoutes.get("/", (request,response)=>{
