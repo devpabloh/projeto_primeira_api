06c86f58a9fef0bba02f68736a054cd671c0ac8b
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-09-11 11:14:19 -0300

    ":sparkles: feat: Adicionando rota de Specifications ao server"

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes.js";
+import { specificationsRoutes } from "./routes/specifications.routes.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes)
+app.use("/specifications", specificationsRoutes)
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
