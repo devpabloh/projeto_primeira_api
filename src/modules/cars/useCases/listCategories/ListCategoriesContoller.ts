3afb5025829a0b389ca5de163dda3b058e714f07
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-09-13 18:17:22 -0300

    "✨ feat: Criando Controller para receber os requests e mandar o response para o index.js"

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesContoller.ts
@@ -0,0 +1,17 @@
+import type {Request, Response} from 'express';
+import { ListCategoriesUseCase } from './ListCategoriesUseCase.js';
+
+class ListCategoriesController{
+    
+    constructor(private listCategoriesUseCase: ListCategoriesUseCase){}
+    
+    handle(request: Request, response: Response):Response{
+        
+
+        const all = this.listCategoriesUseCase.execute()
+
+        return response.json(all)
+    }
+}
+
+export {ListCategoriesController}
