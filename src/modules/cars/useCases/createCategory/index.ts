d9789db6b5079a4fe9c11433f5949b4f4b1cbda8
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-09-23 22:56:21 -0300

    :recycle: refactor: Refatorando a importação do arquivo index.ts

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../../repositories/CategoriesRepository.js";
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository.js";
 import { CreateCategoryController } from "./CreateCategoryController.js";
 import {CreateCategoryUseCase} from "./CreateCategoryUseCase.js";
 
